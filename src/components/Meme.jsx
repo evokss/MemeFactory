1616f897af0e9a73d56052338c9e28c1862ac3c5
Eva <ekoss.prog@gmail.com>
2025-01-04 19:30:33 +0400

    Refactor spacing across all files for consistency

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,45 +1,34 @@
-import React, { useState } from "react"
-import memesData from "../memesData.js"
+import React, { useState } from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
-    const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg"
-    });
-    const [allMemeImages, setallMemeImages] = useState(memesData);
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setallMemeImages] = useState(memesData);
 
-    function getMemeImage() {
-        const memesArray = allMemeImages.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomNumber].url;
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            randomImage: url
-        }))
-    }
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
 
-    return (
-        <main>
-            <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
-                    Get a new meme image 🖼
-                </button>
-            </div>
-            <img src={meme.randomImage} className="meme--image" alt="Meme Image" />
-        </main>
-    )
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={meme.randomImage} className="meme--image" alt="Meme Image" />
+    </main>
+  );
 }
