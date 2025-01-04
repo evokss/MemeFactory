f81646f2cb528a5c0ff6672cd0370591b80d3414
Eva <ekoss.prog@gmail.com>
2025-01-04 19:33:46 +0400

    Update icon route in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" className="header--image" />
+      <img src="./troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Factory</h2>
       <h4 className="header--project">Eva's Projects</h4>
     </header>
