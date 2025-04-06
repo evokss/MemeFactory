8e3f9921781cce78d79b4d83671170fad573f327
Eva <ekoss.prog@gmail.com>
2025-04-06 11:39:44 +0400

    refactor(Header): convert Header component to TypeScript
    - Add TypeScript return type annotation (React.ReactElement)
    - Improve accessibility by adding alt text to header image
    - Maintain all existing functionality and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Header() {
+export default function Header(): React.ReactElement {
   return (
     <header className="header">
-      <img src="./troll-face.png" className="header--image" />
+      <img src="./troll-face.png" className="header--image" alt="Troll Face" />
       <h2 className="header--title">Meme Factory</h2>
       <h4 className="header--project">Eva's Projects</h4>
     </header>
