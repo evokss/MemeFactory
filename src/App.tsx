326e50a2d5fd34cfb2feac0d21fe22a02508fc5f
Eva <ekoss.prog@gmail.com>
2025-04-09 12:12:33 +0400

    refactor(app): migrate App component to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <div>
       <Header />
@@ -10,3 +10,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
